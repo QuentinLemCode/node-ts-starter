c463223af442e42530fa8789e6c5b3c2e513042b
Quentin Lemeasle <quentinlemcode@gmail.com>
2026-01-02 10:06:45 +0000

    test snowflake

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 import { Snowflake } from "@theinternetfolks/snowflake";
+import { Snowflake as ns } from "@sapphire/snowflake";
+import assert from "node:assert";
 
-const ids = Array.from({ length: 10 }, () => Snowflake.generate());
+const sf = new ns(new Date())
+
+const ids = Array.from({ length: 2 }, () => Snowflake.generate());
+const nsIds = Array.from({ length: 2 }, () => sf.generate());
 
 console.log("Generated id " + ids.join(", "));
+console.log("Generated ns id " + nsIds.join(", "));
 
-const parseds = ids.map((id) => Snowflake.parse(id));
+const parsed = sf.decode(nsIds[1]!)
 
-console.log("Parsed ids: " + parseds.map((parsed) => parsed.shard_id).join(", "));
+const { timestamp, workerId, processId, increment, epoch, id } = parsed;
+
+console.log(`Parsed id ${ids[0]}:
+  - timestamp: ${timestamp.toString()}
+  - workerId: ${workerId}
+  - processId: ${processId}
+  - increment: ${increment}
+  - epoch: ${epoch.toString()}
+  - id: ${id}
+`);
